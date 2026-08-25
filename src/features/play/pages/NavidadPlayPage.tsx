4e44ba5709f47e91e2ba1db2e0a73dde7b27e27f
Jose Vicente Martinez Zamora <vicentemaza@gmail.com>
2026-08-26 00:22:50 +0200

    ux: show delivery in special lottery context

--- src/features/play/pages/NavidadPlayPage.tsx
+++ src/features/play/pages/NavidadPlayPage.tsx
@@ -208,6 +208,7 @@ export function NavidadPlayPage({ game }: NavidadPlayPageProps) {
             <div className="sticky top-14 z-10 -mx-4 px-4 pb-2 pt-0">
               <NationalContextBar
                 drawDate={game.nextDraw}
+                delivery={selectedDelivery}
                 method={selectedMethod}
                 onEdit={() => setFlowScreen('config')}
               />
